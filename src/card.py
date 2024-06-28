3589898ef30f1ead8a77dc9e01343d62d6366eb0
__al55__ <jdk0101@pm.me>
2024-06-28 09:56:09 +0800

    Initial commit

---
+++ src/card.py
@@ -0,0 +1,29 @@
+class Card:
+
+    """ Card class describes a card"""
+
+    RANKS = {1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8,
+             8: 9, 9: 10, 10: "JACK", 11: "QUEEN", 12: "KING", 13: "ACE"}
+
+    SUITS = {1: "CLUBS", 2: "DIAMONDS", 3: "HEARTS", 4: "SPADES"}
+
+    def __init__(self, rank, suit):
+        self._validate(rank, Card.RANKS)
+        self._validate(suit, Card.SUITS)
+        self._rank = Card.RANKS[rank]
+        self._suit = Card.SUITS[suit]
+
+    @property
+    def rank(self):
+        return self._rank
+
+    @property
+    def suit(self):
+        return self._suit
+
+    def _validate(self, value, values):
+        if value not in values:
+            raise ValueError(f"VALUE ERROR: {value} is not in {values}.")
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
