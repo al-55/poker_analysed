72ff6832667c46f6a11792acd94b55c74f22f501
AlekLoba <jdk0101@pm.me>
2024-07-07 11:08:24 +0800

    Card.py: attributes made private. Static method was added.

--- src/card.py
+++ src/card.py
@@ -2,34 +2,38 @@ class Card:
 
     """ Card class describes a single card"""
 
-    RANKS = {1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8,
+    __RANKS = {1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8,
              8: 9, 9: 10, 10: "JACK", 11: "QUEEN", 12: "KING", 13: "ACE"}
 
-    SUITS = {1: "CLUBS", 2: "DIAMONDS", 3: "HEARTS", 4: "SPADES"}
+    __SUITS = {1: "CLUBS", 2: "DIAMONDS", 3: "HEARTS", 4: "SPADES"}
 
-    def __init__(self, rank, suit):
-        self._validate(rank, Card.RANKS)
-        self._validate(suit, Card.SUITS)
-        self._rank = Card.RANKS[rank]
-        self._suit = Card.SUITS[suit]
-
-    @property
-    def rank(self):
-        return self._rank
-
-    @property
-    def suit(self):
-        return self._suit
-
-    def _validate(self, value, values):
+    @staticmethod
+    def __validate(value, values):
         if value not in values:
             raise ValueError(f"VALUE ERROR: {value} is not in {values}.")
 
+    def __init__(self, rank, suit):
+        type(self).__validate(rank, type(self).__RANKS)
+        type(self).__validate(suit, type(self).__SUITS)
+        self.__rank = Card.__RANKS[rank]
+        self.__suit = Card.__SUITS[suit]
+
+    def __repr__(self):
+        return f"Card({self.__rank, self.__suit})"
+
+    def __str__(self):
+        return f"Rank: {self.__rank}, Suit: {self.__suit}"
+
     def __eq__(self, other):
-        if self.rank == other.rank and self.suit == other.suit:
+        if self.__rank == other.__rank and self.__suit == other.__suit:
             return True
         else:
             return False
 
-    def __str__(self):
-        return f"{self.rank} of {self.suit}"
+    @property
+    def rank(self):
+        return self.__rank
+
+    @property
+    def suit(self):
+        return self.__suit
